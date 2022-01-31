4976f51b0d0d5c2a7951b288a1964920235a01a4
helixd-2k18 <helixd.2k18@gmail.com>
2022-01-31 15:11:35 +0800

    neo renderer to make

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/VULKAN/NeoRenderer
+# Build directory: C:/VULKAN/NeoRenderer/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
