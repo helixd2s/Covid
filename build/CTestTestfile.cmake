f6333c4c280cbd2860f28c67e1b216336a72c683
helixd-2k18 <helixd.2k18@gmail.com>
2022-02-10 03:46:43 +0800

    rm build

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/VULKAN/NeoRenderer
-# Build directory: C:/VULKAN/NeoRenderer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
