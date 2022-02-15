33cc8c7b268aff0668280304300f83b148560a0f
helixd-2k18 <helixd.2k18@gmail.com>
2022-02-15 13:42:04 +0800

    rename renderer

--- project/include/lxvc/core.hpp
+++ project/include/lxvc/core.hpp
@@ -1,13 +1,14 @@
 #pragma once
 
+// 
 #include <vector>
 
-
+// 
 #include <vulkan/vulkan.h>
 #include <vulkan/vulkan.hpp>
 #include <vulkan-utils/utils.hpp>
 
 // 
-namespace neo {
+namespace lxvc {
   
 };
