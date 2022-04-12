e93481cfa0bf0e357f0f5d2dd4e880e17834e0c1
helixd-2k18 <helixd.2k18@gmail.com>
2022-04-12 23:09:34 +0800

    major update: app class wrap

---
+++ project/test/app.hpp
@@ -0,0 +1,294 @@
+#pragma once
+#define GLM_FORCE_SWIZZLE
+
+//
+#ifdef __cplusplus
+#ifdef _WIN32
+#ifndef VK_USE_PLATFORM_WIN32_KHR
+#define VK_USE_PLATFORM_WIN32_KHR
+#include <windows.h>
+#endif
+#else
+#ifdef __linux__ 
+//FD defaultly
+#endif
+#endif
+
+//
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <Alter/Alter.hpp>
+#include "./controller.hpp"
+
+//
+struct Constants
+{
+  glm::mat4x4 perspective = glm::mat4x4(1.f);
+  glm::mat4x4 perspectiveInverse = glm::mat4x4(1.f);
+  glm::mat3x4 lookAt = glm::mat3x4(1.f);
+  glm::mat3x4 lookAtInverse = glm::mat3x4(1.f);
+};
+
+//
+struct UniformData {
+  uint32_t framebufferAttachments[4] = { 0u,0u,0u,0u };
+  glm::uvec2 extent = {}; uint32_t frameCounter, reserved;
+  Constants constants = {};
+  //uint64_t verticesAddress = 0ull;
+};
+
+// 
+class App {
+protected: 
+  ANAMED::WrapShared<ANAMED::InstanceObj> instanceObj = {};
+  ANAMED::WrapShared<ANAMED::DeviceObj> deviceObj = {};
+  ANAMED::WrapShared<ANAMED::MemoryAllocatorObj> memoryAllocatorVma = {};
+  ANAMED::WrapShared<ANAMED::DescriptorsObj> descriptorsObj = {};
+  ANAMED::WrapShared<ANAMED::UploaderObj> uploaderObj = {};
+  ANAMED::WrapShared<ANAMED::GltfLoaderObj> gltfLoader = {};
+  ANAMED::WrapShared<ANAMED::PipelineObj> computeObj = {};
+  ANAMED::WrapShared<ANAMED::PipelineObj> graphicsObj = {};
+  ANAMED::WrapShared<ANAMED::SwapchainObj> swapchainObj = {};
+  ANAMED::WrapShared<ANAMED::FramebufferObj> framebufferObj = {};
+
+  //
+  UniformData uniformData = {};
+  ANAMED::InstanceAddressBlock instanceAddressBlock = {};
+  ANAMED::QueueGetInfo qfAndQueue = ANAMED::QueueGetInfo{ 0u, 0u };
+  vk::Rect2D renderArea = {};
+
+  //
+  std::shared_ptr<std::array<ANAMED::FenceType, 8>> fences = {};
+  std::shared_ptr<Controller> controller = {};
+  std::shared_ptr<ANAMED::GltfModel> modelObj = {};
+
+  //
+  GLFWwindow* window = nullptr;
+
+  //
+  double previousTime = glfwGetTime();
+  uint32_t frameCount = 0;
+
+  //
+public: 
+  App() {
+    this->construct();
+  };
+
+  //
+  virtual void tickProcessing() {
+    deviceObj->tickProcessing();
+  };
+
+  //
+  virtual void displayFPS(uint32_t& frameCount) {
+    glfwSetWindowTitle(window, (std::string("Alter.TEON.A; FPS: ") + std::to_string(frameCount)).c_str()); frameCount = 0;
+  };
+
+  // 
+  virtual std::experimental::generator<bool> renderGen() {
+    co_yield false;
+
+    // set perspective
+    controller->handleFrame();
+    auto persp = glm::perspective(60.f / 180 * glm::pi<float>(), float(renderArea.extent.width) / float(renderArea.extent.height), 0.001f, 10000.f);
+    auto lkat = glm::lookAt(controller->viewPos, controller->viewCnt, controller->viewUp);
+    uniformData.constants.perspective = glm::transpose(persp);
+    uniformData.constants.perspectiveInverse = glm::transpose(glm::inverse(persp));
+    uniformData.constants.lookAt = glm::mat3x4(glm::transpose(lkat));
+    uniformData.constants.lookAtInverse = glm::mat3x4(glm::transpose(glm::inverse(lkat)));
+    uniformData.extent = glm::uvec2(renderArea.extent.width, renderArea.extent.height);
+    uniformData.frameCounter = 0u;
+
+    //
+//#ifdef ENABLE_RENDERDOC
+    //if (rdoc_api) rdoc_api->StartFrameCapture(NULL, NULL);
+//#endif
+
+    //
+    double currentTime = glfwGetTime();
+    frameCount++;
+
+    if (currentTime - previousTime >= 1.0)
+    {
+      // Display the frame count here any way you want.
+      displayFPS(frameCount);
+      previousTime = currentTime;
+    };
+
+    // 
+    decltype(auto) acquired = swapchainObj->acquireImage(qfAndQueue);
+
+    // wait ready for filling
+    auto& fence = (*fences)[acquired];
+    decltype(auto) status = false;
+    if (fence) { while (!(status = fence->checkStatus())) { co_yield status; }; };
+
+    //
+    uniformData.frameCounter++;
+
+    // 
+    decltype(auto) uniformFence = descriptorsObj->executeUniformUpdateOnce(ANAMED::UniformDataSet{
+      // # yet another std::optional problem (implicit)
+      .writeInfo = std::optional<ANAMED::UniformDataWriteSet>(ANAMED::UniformDataWriteSet{
+        .region = ANAMED::DataRegion{0ull, 4ull, sizeof(UniformData)},
+        .data = cpp21::data_view<char8_t>((char8_t*)&uniformData, 0ull, sizeof(UniformData)),
+      }),
+      .submission = ANAMED::SubmissionInfo{
+        .info = qfAndQueue,
+      }
+    });
+
+    //
+    framebufferObj->clearAttachments(qfAndQueue);
+
+    //
+    decltype(auto) graphicsFence = graphicsObj->executePipelineOnce(ANAMED::ExecutePipelineInfo{
+      // # yet another std::optional problem (implicit)
+      .graphics = std::optional<ANAMED::WriteGraphicsInfo>(ANAMED::WriteGraphicsInfo{
+        .layout = descriptorsObj.as<vk::PipelineLayout>(),
+        .framebuffer = framebufferObj.as<uintptr_t>(),
+        .swapchain = swapchainObj.as<vk::SwapchainKHR>(),
+        .instanceDraws = modelObj->getDefaultScene()->instanced->getDrawInfo(),
+        // # yet another std::optional problem (implicit)
+        .instanceAddressBlock = std::optional<ANAMED::InstanceAddressBlock>(instanceAddressBlock)
+      }),
+      .submission = ANAMED::SubmissionInfo{
+        .info = qfAndQueue,
+      }
+    });
+
+    //
+    decltype(auto) computeFence = computeObj->executePipelineOnce(ANAMED::ExecutePipelineInfo{
+      // # yet another std::optional problem (implicit)
+      .compute = std::optional<ANAMED::WriteComputeInfo>(ANAMED::WriteComputeInfo{
+        .dispatch = vk::Extent3D{cpp21::tiled(renderArea.extent.width, 256u), renderArea.extent.height, 1u},
+        .layout = descriptorsObj.as<vk::PipelineLayout>(),
+        .swapchain = swapchainObj.as<vk::SwapchainKHR>(),
+        // # yet another std::optional problem (implicit)
+        .instanceAddressBlock = std::optional<ANAMED::InstanceAddressBlock>(instanceAddressBlock)
+      }),
+      .submission = ANAMED::SubmissionInfo{
+        .info = qfAndQueue
+      }
+    });
+
+    //
+    fence = std::get<0u>(swapchainObj->presentImage(qfAndQueue));
+
+    // stop the capture
+//#ifdef ENABLE_RENDERDOC
+    //if (rdoc_api) rdoc_api->EndFrameCapture(NULL, NULL);
+//#endif
+
+    // 
+    co_yield true;
+  };
+
+  virtual void initSurface(GLFWwindow* window) {
+    this->controller = std::make_shared<Controller>(this->window = window);
+
+    //
+    vk::SurfaceKHR surface = {};
+    glfwCreateWindowSurface(instanceObj.as<VkInstance>(), window, nullptr, (VkSurfaceKHR*)&surface);
+
+    //
+    swapchainObj = ANAMED::SwapchainObj::make(deviceObj, ANAMED::SwapchainCreateInfo{
+      .layout = descriptorsObj.as<vk::PipelineLayout>(),
+      .surface = surface,
+      .info = qfAndQueue
+      });
+
+    //
+    renderArea = swapchainObj->getRenderArea();
+
+    //
+    framebufferObj = ANAMED::FramebufferObj::make(deviceObj.with(0u), ANAMED::FramebufferCreateInfo{
+      .layout = descriptorsObj.as<vk::PipelineLayout>(),
+      .extent = renderArea.extent,
+      .info = qfAndQueue
+      });
+
+    // 
+    decltype(auto) framebufferAttachments = framebufferObj->getImageViewIndices();
+    memcpy(uniformData.framebufferAttachments, framebufferAttachments.data(), std::min(framebufferAttachments.size(), 4ull) * sizeof(uint32_t));
+  };
+
+  //
+  void loadModel(std::string model, float scale = 1.f) {
+    instanceAddressBlock = ANAMED::InstanceAddressBlock{
+      .opaqueAddressInfo = (modelObj = gltfLoader->load(model, scale))->getDefaultScene()->instanced->getAddressInfo()
+    };
+  };
+
+protected: 
+
+  // 
+  virtual void construct() {
+    uniformData = UniformData{};
+
+    // 
+    instanceObj = ANAMED::InstanceObj::make(ANAMED::context, ANAMED::InstanceCreateInfo{
+
+    });
+
+    // 
+    deviceObj = ANAMED::DeviceObj::make(instanceObj, ANAMED::DeviceCreateInfo{
+      .physicalDeviceGroupIndex = 0u,
+      .physicalDeviceIndex = 0u
+    });
+
+    //
+    memoryAllocatorVma = ANAMED::MemoryAllocatorVma::make(deviceObj, ANAMED::MemoryAllocatorCreateInfo{
+
+    });
+
+    // 
+    descriptorsObj = ANAMED::DescriptorsObj::make(deviceObj.with(0u), ANAMED::DescriptorsCreateInfo{
+
+    });
+
+    //
+    uploaderObj = ANAMED::UploaderObj::make(deviceObj, ANAMED::UploaderCreateInfo{
+
+    });
+
+    //
+    gltfLoader = ANAMED::GltfLoaderObj::make(deviceObj, ANAMED::GltfLoaderCreateInfo{
+      .uploader = uploaderObj.as<uintptr_t>(),
+      .descriptors = descriptorsObj.as<vk::PipelineLayout>()
+    });
+
+    //
+    computeObj = ANAMED::PipelineObj::make(deviceObj.with(0u), ANAMED::PipelineCreateInfo{
+      .layout = descriptorsObj.as<vk::PipelineLayout>(),
+      .compute = ANAMED::ComputePipelineCreateInfo{
+        .code = cpp21::readBinaryU32("./test.comp.spv")
+      }
+    });
+
+    //
+    std::unordered_map<vk::ShaderStageFlagBits, cpp21::shared_vector<uint32_t>> stageMaps = {};
+    stageMaps[vk::ShaderStageFlagBits::eVertex] = cpp21::readBinaryU32("./opaque.vert.spv");
+    stageMaps[vk::ShaderStageFlagBits::eFragment] = cpp21::readBinaryU32("./opaque.frag.spv");
+
+    //
+    graphicsObj = ANAMED::PipelineObj::make(deviceObj.with(0u), ANAMED::PipelineCreateInfo{
+      .layout = descriptorsObj.as<vk::PipelineLayout>(),
+      .graphics = ANAMED::GraphicsPipelineCreateInfo{
+        .stageCodes = stageMaps
+      }
+    });
+
+    //
+    qfAndQueue = ANAMED::QueueGetInfo{ 0u, 0u };
+    fences = std::make_shared<std::array<ANAMED::FenceType, 8>>();
+
+    //
+    previousTime = glfwGetTime();
+    frameCount = 0;
+  };
+
+};
+
+#endif
--- project/test/test.cpp
+++ project/test/test.cpp
@@ -20,7 +20,7 @@
 #include <glm/gtc/matrix_transform.hpp>
 
 // 
-#include <Alter/Alter.hpp>
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #ifdef ENABLE_RENDERDOC
 #include "renderdoc_app.h"
@@ -30,31 +30,13 @@
 //
 #include <args/args.hxx>
 #include "controller.hpp"
+#include "app.hpp"
 
 // 
 void error(int errnum, const char* errmsg)
 {
   std::cerr << errnum << ": " << errmsg << std::endl;
 };
-
-//
-struct Constants
-{
-  glm::mat4x4 perspective = glm::mat4x4(1.f);
-  glm::mat4x4 perspectiveInverse = glm::mat4x4(1.f);
-  glm::mat3x4 lookAt = glm::mat3x4(1.f);
-  glm::mat3x4 lookAtInverse = glm::mat3x4(1.f);
-};
-
-//
-struct UniformData {
-  uint32_t framebufferAttachments[4] = { 0u,0u,0u,0u };
-  glm::uvec2 extent = {}; uint32_t frameCounter, reserved;
-  Constants constants = {};
-  //uint64_t verticesAddress = 0ull;
-};
-
-
 
 // 
 int main(int argc, char** argv) {
@@ -115,83 +97,8 @@ int main(int argc, char** argv) {
 
   //
   ANAMED::initialize();
-
-
-
-
-  //
-  //std::cout << "We running experimental renderer... continue?" << std::endl;
-  //system("PAUSE");
-
-
-  // first cherep
-  decltype(auto) instance = ANAMED::InstanceObj::make(ANAMED::context, ANAMED::InstanceCreateInfo{
-
-  });
-
-  // second cherep
-  decltype(auto) device = ANAMED::DeviceObj::make(instance, ANAMED::DeviceCreateInfo{
-    .physicalDeviceGroupIndex = 0u,
-    .physicalDeviceIndex = 0u
-  });
-
-  //
-  decltype(auto) memoryAllocatorVma = ANAMED::MemoryAllocatorVma::make(device, ANAMED::MemoryAllocatorCreateInfo{
-
-  });
-
-  // final cherep for today
-  decltype(auto) descriptors = ANAMED::DescriptorsObj::make(device.with(0u), ANAMED::DescriptorsCreateInfo{
-
-  });
-
-  //
-  decltype(auto) uniformData = UniformData{};
-
-  //
-  decltype(auto) uploader = ANAMED::UploaderObj::make(device, ANAMED::UploaderCreateInfo{
-
-  });
-
-  //
-  decltype(auto) gltfLoader = ANAMED::GltfLoaderObj::make(device, ANAMED::GltfLoaderCreateInfo{
-    .uploader = uploader.as<uintptr_t>(),
-    .descriptors = descriptors.as<vk::PipelineLayout>()
-  });
-
-  // 
-  decltype(auto) modelObj = gltfLoader->load(args::get(modelflag), args::get(scaleflag));
-
-  //
-  decltype(auto) instanceAddressBlock = ANAMED::InstanceAddressBlock{
-    .opaqueAddressInfo = modelObj->getDefaultScene()->instanced->getAddressInfo()
-  };
-
-  //
-  decltype(auto) compute = ANAMED::PipelineObj::make(device.with(0u), ANAMED::PipelineCreateInfo{
-    .layout = descriptors.as<vk::PipelineLayout>(),
-    .compute = ANAMED::ComputePipelineCreateInfo{
-      .code = cpp21::readBinaryU32("./test.comp.spv")
-    }
-  });
-
-  //
-  std::unordered_map<vk::ShaderStageFlagBits, cpp21::shared_vector<uint32_t>> stageMaps = {};
-  stageMaps[vk::ShaderStageFlagBits::eVertex] = cpp21::readBinaryU32("./opaque.vert.spv");
-  stageMaps[vk::ShaderStageFlagBits::eFragment] = cpp21::readBinaryU32("./opaque.frag.spv");
-
-  //
-  decltype(auto) graphics = ANAMED::PipelineObj::make(device.with(0u), ANAMED::PipelineCreateInfo{
-    .layout = descriptors.as<vk::PipelineLayout>(),
-    .graphics = ANAMED::GraphicsPipelineCreateInfo{
-      .stageCodes = stageMaps
-    }
-  });
-
-  //
-  decltype(auto) qfAndQueue = ANAMED::QueueGetInfo{ 0u, 0u };
-  //std::shared_ptr<std::array<ANAMED::FenceType, 4>> fences = std::make_shared<std::array<ANAMED::FenceType, 4>>();
-  decltype(auto) fences = std::make_shared<std::array<ANAMED::FenceType, 8>>();
+  decltype(auto) app = std::make_shared<App>();
+  app->loadModel(args::get(modelflag), args::get(scaleflag));
 
   //
   glfwSetErrorCallback(error);
@@ -216,164 +123,24 @@ int main(int argc, char** argv) {
   uint32_t SC_WIDTH = WIDTH * xscale, SC_HEIGHT = HEIGHT * yscale;
 
   //
-  vk::SurfaceKHR surface = {};
   std::string title = "Alter.TEON.A";
   decltype(auto) window = glfwCreateWindow(SC_WIDTH, SC_HEIGHT, title.c_str(), nullptr, nullptr);
-  glfwCreateWindowSurface(instance.as<VkInstance>(), window, nullptr, (VkSurfaceKHR*)&surface);
+  
+  //
+  app->initSurface(window);
 
   //
-  decltype(auto) swapchain = ANAMED::SwapchainObj::make(device, ANAMED::SwapchainCreateInfo{
-    .layout = descriptors.as<vk::PipelineLayout>(),
-    .surface = surface,
-    .info = qfAndQueue
-  });
-
-  //
-  decltype(auto) renderArea = swapchain->getRenderArea();
-
-  //
-  Controller controller(window);
-
-  //
-  decltype(auto) framebuffer = ANAMED::FramebufferObj::make(device.with(0u), ANAMED::FramebufferCreateInfo{
-    .layout = descriptors.as<vk::PipelineLayout>(),
-    .extent = renderArea.extent,
-    .info = qfAndQueue
-  });
-
-  //
-  decltype(auto) readySemaphoreInfos = swapchain->getReadySemaphoreInfos();
-  decltype(auto) presentSemaphoreInfos = swapchain->getPresentSemaphoreInfos();
-
-  // 
-  decltype(auto) framebufferAttachments = framebuffer->getImageViewIndices();
-  memcpy(uniformData.framebufferAttachments, framebufferAttachments.data(), std::min(framebufferAttachments.size(), 4ull) * sizeof(uint32_t));
-
-  //
-  std::shared_ptr<std::future<bool>> processing = {};
-
-  //
-  double previousTime = glfwGetTime();
-  int frameCount = 0;
-
-
-
-  //
-  decltype(auto) renderGen = [=, &previousTime, &frameCount, &uniformData, &controller]() -> std::experimental::generator<bool> {
-    co_yield false;
-
-    // set perspective
-    controller.handleFrame();
-    auto persp = glm::perspective(60.f / 180 * glm::pi<float>(), float(renderArea.extent.width) / float(renderArea.extent.height), 0.001f, 10000.f);
-    auto lkat = glm::lookAt(controller.viewPos, controller.viewCnt, controller.viewUp);
-    uniformData.constants.perspective = glm::transpose(persp);
-    uniformData.constants.perspectiveInverse = glm::transpose(glm::inverse(persp));
-    uniformData.constants.lookAt = glm::mat3x4(glm::transpose(lkat));
-    uniformData.constants.lookAtInverse = glm::mat3x4(glm::transpose(glm::inverse(lkat)));
-    uniformData.extent = glm::uvec2(renderArea.extent.width, renderArea.extent.height);
-    uniformData.frameCounter = 0u;
-
-    //
-#ifdef ENABLE_RENDERDOC
-    if (rdoc_api) rdoc_api->StartFrameCapture(NULL, NULL);
-#endif
-
-    //
-    double currentTime = glfwGetTime();
-    frameCount++;
-
-    if (currentTime - previousTime >= 1.0)
-    {
-      // Display the frame count here any way you want.
-      //displayFPS(frameCount);
-      glfwSetWindowTitle(window, (std::string("Alter.TEON.A; FPS: ") + std::to_string(frameCount)).c_str());
-
-      frameCount = 0;
-      previousTime = currentTime;
-    };
-
-    // 
-    decltype(auto) acquired = swapchain->acquireImage(qfAndQueue);
-
-    // wait ready for filling
-    auto& fence = (*fences)[acquired];
-    decltype(auto) status = false;
-    //if (fence) { decltype(auto) unleak = fence->future->get(); }; device->tickProcessing();
-    if (fence) { while (!(status = fence->checkStatus())) { co_yield status; }; };
-
-    //
-    uniformData.frameCounter++;
-
-    // 
-    decltype(auto) uniformFence = descriptors->executeUniformUpdateOnce(ANAMED::UniformDataSet{
-      // # yet another std::optional problem (implicit)
-      .writeInfo = std::optional<ANAMED::UniformDataWriteSet>(ANAMED::UniformDataWriteSet{
-        .region = ANAMED::DataRegion{0ull, 4ull, sizeof(UniformData)},
-        .data = cpp21::data_view<char8_t>((char8_t*)&uniformData, 0ull, sizeof(UniformData)),
-      }),
-      .submission = ANAMED::SubmissionInfo{
-        .info = qfAndQueue,
-      }
-      });
-
-    //
-    framebuffer->clearAttachments(qfAndQueue);
-
-    //
-    decltype(auto) graphicsFence = graphics->executePipelineOnce(ANAMED::ExecutePipelineInfo{
-      // # yet another std::optional problem (implicit)
-      .graphics = std::optional<ANAMED::WriteGraphicsInfo>(ANAMED::WriteGraphicsInfo{
-        .layout = descriptors.as<vk::PipelineLayout>(),
-        .framebuffer = framebuffer.as<uintptr_t>(),
-        .swapchain = swapchain.as<vk::SwapchainKHR>(),
-        .instanceDraws = modelObj->getDefaultScene()->instanced->getDrawInfo(),
-        // # yet another std::optional problem (implicit)
-        .instanceAddressBlock = std::optional<ANAMED::InstanceAddressBlock>(instanceAddressBlock)
-      }),
-      .submission = ANAMED::SubmissionInfo{
-        .info = qfAndQueue,
-      }
-      });
-
-    //
-    decltype(auto) computeFence = compute->executePipelineOnce(ANAMED::ExecutePipelineInfo{
-      // # yet another std::optional problem (implicit)
-      .compute = std::optional<ANAMED::WriteComputeInfo>(ANAMED::WriteComputeInfo{
-        .dispatch = vk::Extent3D{cpp21::tiled(renderArea.extent.width, 256u), renderArea.extent.height, 1u},
-        .layout = descriptors.as<vk::PipelineLayout>(),
-        .swapchain = swapchain.as<vk::SwapchainKHR>(),
-        // # yet another std::optional problem (implicit)
-        .instanceAddressBlock = std::optional<ANAMED::InstanceAddressBlock>(instanceAddressBlock)
-      }),
-      .submission = ANAMED::SubmissionInfo{
-        .info = qfAndQueue
-      }
-      });
-
-    //
-    fence = std::get<0u>(swapchain->presentImage(qfAndQueue));
-
-    // stop the capture
-#ifdef ENABLE_RENDERDOC
-    if (rdoc_api) rdoc_api->EndFrameCapture(NULL, NULL);
-#endif
-
-    // 
-    co_yield true;
-  };
-
-  //
-  decltype(auto) rendering = renderGen();
+  decltype(auto) rendering = app->renderGen();
   decltype(auto) iterator = rendering.begin();
 
   // 
   while (!glfwWindowShouldClose(window)) { // 
     glfwPollEvents();
-    device->tickProcessing();
+    app->tickProcessing();
     _CrtDumpMemoryLeaks();
 
     //
-    if (iterator == rendering.end()) { rendering = renderGen(), iterator = rendering.begin(); };
+    if (iterator == rendering.end()) { rendering = app->renderGen(), iterator = rendering.begin(); };
     iterator++;
   };
 
