28d02e278e23333cc8ea492b788a7b229f8d418b
helixd-2k18 <helixd.2k18@gmail.com>
2022-04-24 01:01:09 +0800

    optimize SPIR-V (but no boost)

--- project/test/compile.cmd
+++ project/test/compile.cmd
@@ -5,4 +5,13 @@ glslangValidator --target-env spirv1.6 --client vulkan100 rasterize.frag -o opaq
 glslangValidator --target-env spirv1.6 --client vulkan100 rasterize.vert -o opaque.vert.spv
 glslangValidator --target-env spirv1.6 --client vulkan100 rasterize.frag -DTRANSLUCENT -o translucent.frag.spv
 glslangValidator --target-env spirv1.6 --client vulkan100 rasterize.vert -DTRANSLUCENT -o translucent.vert.spv
+
+spirv-opt --skip-validation -O test.comp.spv -o test.comp.spv
+spirv-opt --skip-validation -O post.comp.spv -o post.comp.spv
+spirv-opt --skip-validation -O resample.comp.spv -o resample.comp.spv
+spirv-opt --skip-validation -O opaque.frag.spv -o opaque.frag.spv
+spirv-opt --skip-validation -O opaque.vert.spv -o opaque.vert.spv
+spirv-opt --skip-validation -O translucent.frag.spv -o translucent.frag.spv
+spirv-opt --skip-validation -O translucent.vert.spv -o translucent.vert.spv
+
 pause
