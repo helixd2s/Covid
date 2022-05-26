cbad9a1f7da7e5742eef299174e908bfb84011e7
helixd-2k18 <helixd.2k18@gmail.com>
2022-05-26 13:53:29 +0800

    SSAAx4 for 1x scale

--- project/shaders/lib/native.glsl
+++ project/shaders/lib/native.glsl
@@ -88,6 +88,20 @@ layout(set = 3, binding = 0, r32ui) uniform uimage2D imagesR32UI[];
 layout(set = 3, binding = 0, r32f) uniform image2D imagesR32F[];
 
 //
+vec4 textureBilinear( in uint image, in vec2 uv )
+{
+  const vec2 res = imageSize( imagesRgba32F[image] );
+  const vec2 st = uv*res - 0.5;
+  const vec2 iuv = floor( st );
+  const vec2 fuv = fract( st );
+  const vec4 a = imageLoad( imagesRgba32F[image], ivec2(iuv+vec2(0.5,0.5)) );
+  const vec4 b = imageLoad( imagesRgba32F[image], ivec2(iuv+vec2(1.5,0.5)) );
+  const vec4 c = imageLoad( imagesRgba32F[image], ivec2(iuv+vec2(0.5,1.5)) );
+  const vec4 d = imageLoad( imagesRgba32F[image], ivec2(iuv+vec2(1.5,1.5)) );
+  return mix( mix( a, b, fuv.x), mix( c, d, fuv.x), fuv.y );
+};
+
+//
 #include "./constants.glsl"
 #include "./utils.glsl"
 #include "./interface.glsl"
